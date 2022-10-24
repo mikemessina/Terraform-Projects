5c673274f654e2df2c6cd392792f05648f055b67
Michael Messina <michael.messina@appliedis.com>
2022-10-24 16:20:22 -0400

    output update

--- ADO/AKS/basic-cluster/outputs.tf
+++ ADO/AKS/basic-cluster/outputs.tf
@@ -4,18 +4,22 @@ output "id" {
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  sensitive = true
 }
 
 output "client_key" {
   value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
+  sensitive = true
 }
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
   value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
+  sensitive = true
 }
 
 output "host" {
